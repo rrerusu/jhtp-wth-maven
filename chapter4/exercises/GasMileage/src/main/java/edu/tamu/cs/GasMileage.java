a395ec6937da117708abce168b5b067f18ccfd95
rrerusu <rrerusu@gmail.com>
2022-02-11 08:49:52 -0600

    4.17 Calculate Gas Mileage, use Sentinel Control

---
+++ chapter4/exercises/GasMileage/src/main/java/edu/tamu/cs/GasMileage.java
@@ -0,0 +1,35 @@
+package edu.tamu.cs;
+
+import java.util.Scanner;
+
+public class GasMileage {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int gallons = 0, miles = 0, totalGallons = 0, totalMiles = 0;
+
+        while(gallons != -1 || miles != -1) {
+            System.out.printf("Enter miles driven: ");
+            miles = input.nextInt();
+
+            if(miles != -1) {
+                System.out.printf("Enter gallons driven: ");
+                gallons = input.nextInt();
+
+                if(gallons != -1) {
+                    totalMiles += miles;
+                    totalGallons += gallons;
+                    System.out.printf("MPG for this trip was %.2f\n\n", miles / gallons);
+                }
+            }
+        }
+
+        if(totalGallons == 0 || totalMiles == 0)
+            System.out.printf("Exiting program.");
+        else
+            System.out.printf("Total miles for all trips: %d\nTotal gallons for all trips: %d\nTotal MPG: %.2f",
+                totalMiles, totalGallons, totalMiles / totalGallons);
+
+        input.close();
+    }
+}
