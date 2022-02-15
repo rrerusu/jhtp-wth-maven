c3459a5181e6697c8ed9185e778c45402509f558
rrerusu <rrerusu@gmail.com>
2022-02-14 18:18:03 -0600

    Test break statements

---
+++ chapter5/examples/BreakTest/src/main/java/edu/tamu/cs/BreakTest.java
@@ -0,0 +1,16 @@
+package edu.tamu.cs;
+
+public class BreakTest {
+    public static void main(String[] args) {
+        int count;
+
+        for(count = 1; count <= 10; count++) {
+            if(count == 5)
+                break;
+            
+            System.out.printf("%d   ", count);
+        }
+
+        System.out.printf("\nBroke out of a loop at count = %d\n", count);
+    }
+}
