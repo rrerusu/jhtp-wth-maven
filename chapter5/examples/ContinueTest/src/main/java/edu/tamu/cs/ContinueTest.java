2ca1d63288bc3d0a2b52cce2496124161b906488
rrerusu <rrerusu@gmail.com>
2022-02-14 18:24:48 -0600

    Test funcionality of the key word continue

---
+++ chapter5/examples/ContinueTest/src/main/java/edu/tamu/cs/ContinueTest.java
@@ -0,0 +1,14 @@
+package edu.tamu.cs;
+
+public class ContinueTest {
+    public static void main(String[] args) {
+        for(int count = 1; count <= 10; count++) {
+            if(count == 5)
+                continue;
+
+            System.out.printf("%d   ", count);
+        }
+
+        System.out.printf("\nUsed continue to skip representing 5\n");
+    }
+}
