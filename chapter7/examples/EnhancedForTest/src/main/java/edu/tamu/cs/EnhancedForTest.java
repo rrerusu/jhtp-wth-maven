a37cda3a13d10522d659655186bd8a421b339fb8
rrerusu <rrerusu@gmail.com>
2022-03-02 10:39:12 -0600

    Pass arrays into methods and modify them

--- chapter7/examples/EnhancedForTest/src/main/java/edu/tamu/cs/EnhancedForTest.java
+++ chapter7/examples/EnhancedForTest/src/main/java/edu/tamu/cs/EnhancedForTest.java
@@ -4,6 +4,10 @@ package edu.tamu.cs;
  * Test funcionalities of enhanced for loop
  */
 public class EnhancedForTest {
+    /**
+     * Main method to run code
+     * @param args Command line arguments
+     */
     public static void main(String[] args) {
         int[] array = {87, 68, 94, 100, 83, 78, 85, 91, 76, 87};
         int total = 0;
---
+++ chapter7/examples/PassArray/src/main/java/edu/tamu/cs/PassArray.java
@@ -0,0 +1,42 @@
+package edu.tamu.cs;
+
+/**
+ * Passing array and individual elements to methods
+ */
+public class PassArray {
+    /**
+     * Main method to run code
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+
+        System.out.printf("Effects of passing reference to entire array:\n" +
+            "The values  of the original array are:\n");
+        
+        for(int value : array)
+            System.out.printf("    %d", value);
+        
+        modifyArray(array);
+        System.out.printf("\n\nThe values of the modified array are:\n");
+
+        for(int value : array)
+            System.out.printf("    %d", value);
+        
+        System.out.printf("\n\nEffects of passing array element value:\n" +
+            "array[3] before modifyingElement: %d\n", array[3]);
+        
+        modifyElement(array[3]);
+        System.out.printf("array[3] after modifyElement: %d\n", array[3]);
+    }
+
+    public static void modifyArray(int[] array2) {
+        for(int counter = 0; counter < array2.length; counter++)
+            array2[counter] *= 2;
+    }
+
+    public static void modifyElement(int element) {
+        element *= 2;
+        System.out.printf("Value of element in modifyElement: %d\n", element);
+    }
+}
