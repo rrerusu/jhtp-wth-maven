5b93a5094ac364d308ff940b9fe584f45e635f56
rrerusu <rrerusu@gmail.com>
2022-03-18 14:29:07 -0500

    Include static counter variable to keep track of number of employees created

--- chapter8/examples/Employee/src/main/java/edu/tamu/cs/Employee.java
+++ chapter8/examples/Employee/src/main/java/edu/tamu/cs/Employee.java
@@ -5,6 +5,10 @@ package edu.tamu.cs;
  */
 public class Employee {
     /**
+     * Number of employees created
+     */
+    private static int count = 0;
+    /**
      * First name of the employee
      */
     private String firstName;
@@ -12,36 +16,65 @@ public class Employee {
      * Last name of the employee
      */
     private String lastName;
-    /**
-     * Employee birth date
-     */
-    private Date birthDate;
-    /**
-     * Date that employee was hired
-     */
-    private Date hireDate;
+    // /**
+    //  * Employee birth date
+    //  */
+    // private Date birthDate;
+    // /**
+    //  * Date that employee was hired
+    //  */
+    // private Date hireDate;
 
     /**
      * Costructor that takes all values
      * @param firstName First name of employee
      * @param lastName Last name of employee
-     * @param birthDate Birth date of employee
-     * @param hireDate Hire date of employee
      */
-    public Employee(String firstName, String lastName, Date birthDate,
-        Date hireDate) {
+    //  * @param birthDate Birth date of employee
+    //  * @param hireDate Hire date of employee
+    //  */
+    public Employee(String firstName, String lastName/*, Date birthDate,*/
+        /*Date hireDate*/) {
             this.firstName = firstName;
             this.lastName = lastName;
-            this.birthDate = birthDate;
-            this.hireDate = hireDate;
+
+            ++count;
+            System.out.printf("Employees constructor: %s %s; count = %d\n",
+                firstName, lastName, count);
+            // this.birthDate = birthDate;
+            // this.hireDate = hireDate;
     }
 
     /**
-     * Create String representation of this Employee object
-     * @return String representation of this Employee object
+     * Accessor method for first name
+     * @return Employee's first name
      */
-    public String toString() {
-        return String.format("%s, %s  Hired: %s  Birthday: %s",
-            lastName, firstName, hireDate, birthDate);
+    public String getFirstName() {
+        return firstName;
     }
+
+    /**
+     * Accessor method for last name
+     * @return Employee's last name
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * Accessor method for number of employees created
+     * @return Number of employees created
+     */
+    public static int getCount() {
+        return count;
+    }
+
+    // /**
+    //  * Create String representation of this Employee object
+    //  * @return String representation of this Employee object
+    //  */
+    // public String toString() {
+    //     return String.format("%s, %s  Hired: %s  Birthday: %s",
+    //         lastName, firstName, hireDate, birthDate);
+    // }
 }
